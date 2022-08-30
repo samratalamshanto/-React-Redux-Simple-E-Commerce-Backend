a069f2fc62fc2d1cffb2108a77a6799b7cfe39a4
samratalam <samratalamshanto710@gmail.com>
2022-08-30 20:58:08 +0600

    .

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cartRouter = require("./server_side_component/router/cartRouter");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-    origin: "http://localhost:3000",//`https://samrat-alam.herokuapp.com:${port}`,
+    origin: `https://ecommece-websites.herokuapp.com/:${port}`,//"http://localhost:3000"
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 };
@@ -21,13 +21,29 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+
 mongoose.connect(process.env.MONGOOSE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    console.log(
-        "Connected"
-    );
+    // console.log(
+    //     "Connected"
+    // );
 }).catch((err) => console.log(err));
 
 
